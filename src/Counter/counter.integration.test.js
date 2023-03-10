42d8c553f888b0089f5eac00fcc102d22bca1e22
Freddy Contreras <freddycontreras3@gmail.com>
2023-03-10 11:36:28 -0500

    feat: implement integrations test

--- src/Counter/counter.integration.test.js
+++ src/Counter/counter.integration.test.js
@@ -1,7 +1,34 @@
-describe("Probar el correcto funcionamiento del contador", () => {
-  test.todo("Debe aumentar el contador a 1, luego de hacer click en 'Incrementar'");
+import { fireEvent, render, screen } from "@testing-library/react";
+import Counter from ".";
 
-  test.todo("El contador debe decrementar en 1, luego de hacer click en 'Decrementar'");
-
-  test.todo("El contador debe reiniciar a 0, luego de hacer click en 'Reiniciar'");
+beforeEach(() => {
+  render(<Counter />);
 });
+
+describe("Probar el correcto funcionamiento del contador", () =>  {
+  test("Debe aumentar el contador a 1, luego de hacer click en 'Incrementar'", () => {
+    const button = screen.getByRole("button", { name: /incrementar/i});
+    fireEvent.click(button);
+    expect(screen.getByText(/contador: 1/i)).toBeInTheDocument();
+  });
+
+  test("El contador debe decrementar en 1, luego de hacer click en 'Decrementar'", () => {
+    const button = screen.getByRole("button", { name: /decrementar/i});
+    fireEvent.click(button);
+    expect(screen.getByText(/contador: -1/i)).toBeInTheDocument();
+  })
+
+  test("El contador debe reiniciar a 0, luego de hacer click en 'Reiniciar'", () => {
+    const buttonAdd = screen.getByRole("button", { name: /incrementar/i});
+    fireEvent.click(buttonAdd);
+    
+    const buttonReset = screen.getByRole("button", { name: /reiniciar/i});
+    fireEvent.click(buttonReset);
+    
+    expect(screen.getByText(/contador: 0/i)).toBeInTheDocument();
+
+    const buttonSubtract = screen.getByRole("button", { name: /incrementar/i});
+    fireEvent.click(buttonSubtract);
+    
+  })
+})
--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Counter = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <>
       <h1>App Counter</h1>
-      <p>Contador: 0</p>
-      <button>Incrementar</button>
-      <button>Decrementar</button>
-      <button>Reiniciar</button>
+      <p>Contador: {counter}</p>
+      <button onClick={() => setCounter(counter+1)}>Incrementar</button>
+      <button onClick={() => setCounter(counter-1)}>Decrementar</button>
+      <button onClick={() => setCounter(0)}>Reiniciar</button>
     </>
   );
 }
