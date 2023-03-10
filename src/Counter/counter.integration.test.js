820914dee4d8bf8eeaed197526afda20f88a7019
Freddy Contreras <freddycontreras3@gmail.com>
2023-03-10 11:38:11 -0500

    feat: refactor integration test

--- src/Counter/counter.integration.test.js
+++ src/Counter/counter.integration.test.js
@@ -1,34 +1,37 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from ".";
 
+let buttonAdd;
+let buttonSubtract;
+let buttonReset;
+
 beforeEach(() => {
   render(<Counter />);
+  buttonAdd = screen.getByRole("button", { name: /incrementar/i });
+  buttonSubtract = screen.getByRole("button", { name: /decrementar/i });
+  buttonReset = screen.getByRole("button", { name: /reiniciar/i });
 });
 
-describe("Probar el correcto funcionamiento del contador", () =>  {
+describe("Probar el correcto funcionamiento del contador", () => {
   test("Debe aumentar el contador a 1, luego de hacer click en 'Incrementar'", () => {
-    const button = screen.getByRole("button", { name: /incrementar/i});
-    fireEvent.click(button);
+    fireEvent.click(buttonAdd);
     expect(screen.getByText(/contador: 1/i)).toBeInTheDocument();
   });
 
   test("El contador debe decrementar en 1, luego de hacer click en 'Decrementar'", () => {
-    const button = screen.getByRole("button", { name: /decrementar/i});
-    fireEvent.click(button);
+    fireEvent.click(buttonSubtract);
     expect(screen.getByText(/contador: -1/i)).toBeInTheDocument();
-  })
+  });
 
   test("El contador debe reiniciar a 0, luego de hacer click en 'Reiniciar'", () => {
-    const buttonAdd = screen.getByRole("button", { name: /incrementar/i});
     fireEvent.click(buttonAdd);
-    
-    const buttonReset = screen.getByRole("button", { name: /reiniciar/i});
     fireEvent.click(buttonReset);
-    
+
     expect(screen.getByText(/contador: 0/i)).toBeInTheDocument();
 
-    const buttonSubtract = screen.getByRole("button", { name: /incrementar/i});
     fireEvent.click(buttonSubtract);
-    
-  })
-})
+    fireEvent.click(buttonReset);
+
+    expect(screen.getByText(/contador: 0/i)).toBeInTheDocument();
+  });
+});
