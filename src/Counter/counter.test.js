f0a583e3b8ccd363143c1d8495bb9b5b7a7f6040
Freddy Contreras <freddycontreras3@gmail.com>
2023-03-10 11:35:15 -0500

    feat: refactor testing

--- src/Counter/counter.test.js
+++ src/Counter/counter.test.js
@@ -1,35 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import Counter from ".";
 
+beforeEach(() => {
+  render(<Counter />);
+});
+
 describe("Pruebas unitarias de mi contador", () => {
   test("Verificar el título de la página", () => {
-    render(<Counter />);
     expect(screen.getByText(/app counter/i)).toBeInTheDocument();
   });
 
   test("Verificar si existe el botón 'Incrementar'", () => {
-    render(<Counter />);
     expect(
       screen.getByRole("button", { name: /incrementar/i })
     ).toBeInTheDocument();
   });
 
   test("Verificar si existe el botón 'Decrementar'", () => {
-    render(<Counter />);
     expect(
       screen.getByRole("button", { name: /decrementar/i })
     ).toBeInTheDocument();
   });
 
   test("Verificar si existe el botón 'Reiniciar'", () => {
-    render(<Counter />);
     expect(
       screen.getByRole("button", { name: /reiniciar/i })
     ).toBeInTheDocument();
   });
 
   test("Verificar que el contador se encuentre en cero (0)", () => {
-    render(<Counter />);
     expect(screen.getByText(/contador: 0/i)).toBeInTheDocument();
   });
 });
