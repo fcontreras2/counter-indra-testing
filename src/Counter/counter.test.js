6c9fd36ec79f8635a3ab98ad1f2604772b888f31
Freddy Contreras <freddycontreras3@gmail.com>
2023-03-10 11:34:29 -0500

    feat: testing to render elements in the document

--- src/Counter/counter.test.js
+++ src/Counter/counter.test.js
@@ -1,11 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import Counter from ".";
+
 describe("Pruebas unitarias de mi contador", () => {
-  test.todo("Verificar el título de la página");
+  test("Verificar el título de la página", () => {
+    render(<Counter />);
+    expect(screen.getByText(/app counter/i)).toBeInTheDocument();
+  });
 
-  test.todo("Verificar si existe el botón 'Incrementar'");
+  test("Verificar si existe el botón 'Incrementar'", () => {
+    render(<Counter />);
+    expect(
+      screen.getByRole("button", { name: /incrementar/i })
+    ).toBeInTheDocument();
+  });
 
-  test.todo("Verificar si existe el botón 'Decrementar'");
+  test("Verificar si existe el botón 'Decrementar'", () => {
+    render(<Counter />);
+    expect(
+      screen.getByRole("button", { name: /decrementar/i })
+    ).toBeInTheDocument();
+  });
 
-  test.todo("Verificar si existe el botón 'Reiniciar'");
+  test("Verificar si existe el botón 'Reiniciar'", () => {
+    render(<Counter />);
+    expect(
+      screen.getByRole("button", { name: /reiniciar/i })
+    ).toBeInTheDocument();
+  });
 
-  test.todo("Verificar que el contador se encuentre en cero (0)");
+  test("Verificar que el contador se encuentre en cero (0)", () => {
+    render(<Counter />);
+    expect(screen.getByText(/contador: 0/i)).toBeInTheDocument();
+  });
 });
--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,6 +1,13 @@
 const Counter = () => {
-
-  return null;
+  return (
+    <>
+      <h1>App Counter</h1>
+      <p>Contador: 0</p>
+      <button>Incrementar</button>
+      <button>Decrementar</button>
+      <button>Reiniciar</button>
+    </>
+  );
 }
 
 export default Counter;
